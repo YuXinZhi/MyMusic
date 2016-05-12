32efd2d4cd79fe552b8567ee0c340504bf3eeca4
Ben <whoyumengmeng@gmail.com>
2016-05-12 22:00:42 +0800

    加了IMusicPalybackService

--- src/com/example/mymusic/MusicUtils.java
+++ src/com/example/mymusic/MusicUtils.java
@@ -1,5 +1,71 @@
 package com.example.mymusic;
 
+import java.util.Formatter;
+import java.util.Locale;
+
+import com.android.music.IMediaPlaybackService;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
 public class MusicUtils {
 
+	public static Cursor query(Context context, Uri uri, String[] projection, String selection, String[] selectionArgs,
+			String sortOrder) {
+
+		return query(context, uri, projection, selection, selectionArgs, sortOrder, 0);
+	}
+
+	public static Cursor query(Context context, Uri uri, String[] projection, String selection, String[] selectionArgs,
+			String sortOrder, int limit) {
+		try {
+			ContentResolver resolver = context.getContentResolver();
+			if (resolver == null) {
+				return null;
+			}
+
+			if (limit > 0) {
+				uri = uri.buildUpon().appendQueryParameter("limit", "" + limit).build();
+			}
+
+			return resolver.query(uri, projection, selection, selectionArgs, sortOrder);
+		} catch (UnsupportedOperationException ex) {
+			// 查询条件错误
+		}
+		return null;
+	}
+
+	/*
+	 * Try to use String.format() as little as possible, because it creates a
+	 * new Formatter every time you call it, which is very inefficient. Reusing
+	 * an existing Formatter more than tripled the speed of makeTimeString().
+	 * This Formatter/StringBuilder are also used by makeAlbumSongsLabel()
+	 */
+	private static StringBuilder sFormatBuilder = new StringBuilder();
+	private static Formatter sFormatter = new Formatter(sFormatBuilder, Locale.getDefault());
+	private static final Object[] sTimeArgs = new Object[5];
+
+	public static String makeTimeString(Context context, int secs) {
+		String durationformat = context
+				.getString(secs < 3600 ? R.string.durationformatshort : R.string.durationformatlong);
+
+		/*
+		 * Provide multiple arguments so the format can be changed easily by
+		 * modifying the xml.
+		 */
+		sFormatBuilder.setLength(0);
+
+		final Object[] timeArgs = sTimeArgs;
+		timeArgs[0] = secs / 3600;
+		timeArgs[1] = secs / 60;
+		timeArgs[2] = (secs / 60) % 60;
+		timeArgs[3] = secs;
+		timeArgs[4] = secs % 60;
+
+		return sFormatter.format(durationformat, timeArgs).toString();
+	}
+	
+	public static IMediaPlaybackService sService = null;
 }
--- src/com/example/mymusic/TrackBrowserActivity.java
+++ src/com/example/mymusic/TrackBrowserActivity.java
@@ -1,10 +1,17 @@
 package com.example.mymusic;
 
 import android.app.ListActivity;
+import android.content.AsyncQueryHandler;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.CharArrayBuffer;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.ContactsContract.CommonDataKinds.Im;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -38,8 +45,20 @@ public class TrackBrowserActivity extends ListActivity {
 	}
 
 	static class TrackListAdapter extends SimpleCursorAdapter {
-		boolean mIsNowPlaying;//
+		boolean mIsNowPlaying;
 		boolean mDisableNowPlayingIndicator;
+
+		int mTitleIdx;
+		int mArtistIdx;
+		int mDurationIdx;
+		int mAudioIdIdx;
+
+		private final StringBuilder mBuilder = new StringBuilder();
+		private final String mUnknownArtist;
+		private final String mUnknownAlbum;
+
+		private TrackBrowserActivity mActivity = null;
+		private TrackQueryHandler mQueryHandler;
 
 		static class ViewHolder {
 			TextView song;
@@ -50,11 +69,176 @@ public class TrackBrowserActivity extends ListActivity {
 			char[] buffer2;
 		}
 
-		public TrackListAdapter(Context context, int layout, Cursor cursor, String[] from, int[] to,
-				boolean isnowplaying, boolean disablenowplayingindicator) {
-			super(context, layout, cursor, from, to);
+		class TrackQueryHandler extends AsyncQueryHandler {
+
+			class QueryArgs {
+				public Uri uri;
+				public String[] projection;
+				public String selection;
+				public String[] selectionArgs;
+				public String orderBy;
+			}
+
+			public TrackQueryHandler(ContentResolver cr) {
+				super(cr);
+			}
+
+			/**
+			 * 
+			 * @param uri
+			 * @param projection
+			 *            查询的条目
+			 * @param selection
+			 *            查询条件
+			 * @param selectionArgs
+			 *            查询条件中占位符参数
+			 * @param orderBy
+			 * @param async
+			 *            是否是异步查询
+			 */
+
+			public Cursor doQuery(Uri uri, String[] projection, String selection, String[] selectionArgs,
+					String orderBy, boolean async) {
+				if (async) {
+					Uri limituri = uri.buildUpon().appendQueryParameter("limit", "100").build();
+					QueryArgs args = new QueryArgs();
+					args.uri = uri;
+					args.projection = projection;
+					args.selection = selection;
+					args.selectionArgs = selectionArgs;
+					args.orderBy = orderBy;
+					// 交给handler处理查询,异步查询
+					startQuery(0, args, limituri, projection, selection, selectionArgs, orderBy);
+					return null;
+				}
+				return MusicUtils.query(mActivity, uri, projection, selection, selectionArgs, orderBy);
+			}
+
+			/**
+			 * token: the token to identify the query, passed in from
+			 * startQuery.
+			 * 
+			 * cookie: the cookie object passed in from startQuery.
+			 * 
+			 * cursor :The cursor holding the results from the query.
+			 */
+
+			@Override
+			protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+				mActivity.init(cursor, cookie != null);// 当cookie!=null说明limit是100，不止一条记录
+
+			}
 
 		}
+
+		@SuppressWarnings("deprecation")
+		public TrackListAdapter(Context context, TrackBrowserActivity currentactivity, int layout, Cursor cursor,
+				String[] from, int[] to, boolean isnowplaying, boolean disablenowplayingindicator) {
+			super(context, layout, cursor, from, to);
+			mActivity = currentactivity;
+			getColums(cursor);
+			mIsNowPlaying = isnowplaying;
+			mDisableNowPlayingIndicator = disablenowplayingindicator;
+			mUnknownArtist = context.getString(R.string.unknown_artist_name);
+			mUnknownAlbum = context.getString(R.string.unknown_album_name);
+
+			mQueryHandler = new TrackQueryHandler(context.getContentResolver());
+		}
+
+		public void setActivity(TrackBrowserActivity newactivity) {
+			mActivity = newactivity;
+		}
+
+		public TrackQueryHandler getQueryHanlder() {
+			return mQueryHandler;
+		}
+
+		// 设置查询结果赋值给
+		private void getColums(Cursor cursor) {
+			if (cursor != null) {
+				mTitleIdx = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+				mArtistIdx = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST);
+				mDurationIdx = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION);
+				try {
+					mAudioIdIdx = cursor.getColumnIndexOrThrow(MediaStore.Audio.Playlists.Members.AUDIO_ID);
+				} catch (IllegalArgumentException ex) {
+					mAudioIdIdx = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID);
+				}
+			}
+		}
+
+		/**
+		 * Inflates view(s) from the specified XML file.
+		 */
+		@Override
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
+			View view = super.newView(context, cursor, parent);
+			ImageView iv = (ImageView) view.findViewById(R.id.icon);
+			iv.setVisibility(View.GONE);
+
+			ViewHolder vh = new ViewHolder();
+			vh.song = (TextView) view.findViewById(R.id.song);
+			vh.artist = (TextView) view.findViewById(R.id.artist);
+			vh.duration = (TextView) view.findViewById(R.id.duration);
+			vh.play_indicator = (ImageView) view.findViewById(R.id.play_indicator);
+			vh.buffer1 = new CharArrayBuffer(100);
+			vh.buffer2 = new char[200];
+			view.setTag(vh);
+
+			return view;
+		}
+
+		/**
+		 * Binds all of the field names passed into the "to" parameter of the
+		 * constructor with their corresponding cursor columns as specified in
+		 * the "from" parameter.
+		 */
+		@Override
+		public void bindView(View view, Context context, Cursor cursor) {
+			ViewHolder vh = (ViewHolder) view.getTag();
+			// 把mTitleIdx列的值赋给vh.buffer1
+			cursor.copyStringToBuffer(mTitleIdx, vh.buffer1);
+			vh.song.setText(vh.buffer1.data, 0, vh.buffer1.sizeCopied);
+
+			// 歌曲持续时间转换成秒
+			int secs = cursor.getInt(mDurationIdx) / 1000;
+			if (secs == 0) {
+				vh.duration.setText("");
+
+			} else {
+				vh.duration.setTag(MusicUtils.makeTimeString(context, secs));
+			}
+
+			final StringBuilder builder = mBuilder;
+			builder.delete(0, builder.length());
+
+			String name = cursor.getString(mArtistIdx);
+			if (name == null || name.equals(MediaStore.UNKNOWN_STRING)) {
+				builder.append(mUnknownAlbum);
+			} else {
+				builder.append(name);
+			}
+
+			int len = builder.length();
+			if (vh.buffer2.length < len) {
+				vh.buffer2 = new char[len];
+			}
+
+			builder.getChars(0, len, vh.buffer2, 0);
+			vh.artist.setText(vh.buffer2, 0, len);
+			
+			ImageView iv=vh.play_indicator;
+			long id=-1;
+			
+			if (MusicUtils.sService) {
+				
+			}
+		}
+		
+
+	}
+
+	public void init(Cursor newCursor, boolean isLimited) {
 
 	}
 }
