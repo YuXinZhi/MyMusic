b51c170c9126210239f9af0b9f39ab8ae00e36dd
Ben <whoyumengmeng@gmail.com>
2016-05-12 17:28:33 +0800

    删除国际化的vlue目录

---
+++ src/com/example/mymusic/ui/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.example.mymusic.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
