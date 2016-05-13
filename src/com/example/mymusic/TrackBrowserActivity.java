ce708795ae2b4dea15f6f93865b2ff401c49808a
Ben <whoyumengmeng@gmail.com>
2016-05-13 10:43:43 +0800

    分析startquery

--- src/com/example/mymusic/TrackBrowserActivity.java
+++ src/com/example/mymusic/TrackBrowserActivity.java
@@ -10,6 +10,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -40,52 +42,115 @@ public class TrackBrowserActivity extends ListActivity {
 
 	private String mPlaylist;
 
+	private String mSortOrder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.media_picker_activity);
-
-		mAdapter = new TrackListAdapter(getApplication(), // need to use
-				// application
-				// context to
-				// avoid leaks
-				this, R.layout.track_list_item, null, // cursor
-				new String[] {}, new int[] {}, "nowplaying".equals(mPlaylist),
-				mPlaylist != null && !(mPlaylist.equals("podcasts") || mPlaylist.equals("recentlyadded")));
+		mAdapter.setActivity(this);
 		setListAdapter(mAdapter);
 		setTitle(R.string.working_songs);
-		getTrackCursor(mAdapter.getQueryHandler(), null, true);
-
-		// if (mAdapter == null) {
-		// mAdapter = new TrackListAdapter(getApplication(), // need to use
-		// // application
-		// // context to
-		// // avoid leaks
-		// this, R.layout.track_list_item, null, // cursor
-		// new String[] {}, new int[] {}, "nowplaying".equals(mPlaylist),
-		// mPlaylist != null && !(mPlaylist.equals("podcasts") ||
-		// mPlaylist.equals("recentlyadded")));
-		// setListAdapter(mAdapter);
-		// setTitle(R.string.working_songs);
-		// getTrackCursor(mAdapter.getQueryHandler(), null, true);
-		// } else {
-		// mTrackCursor = mAdapter.getCursor();
-		// // If mTrackCursor is null, this can be because it doesn't have
-		// // a cursor yet (because the initial query that sets its cursor
-		// // is still in progress), or because the query failed.
-		// // In order to not flash the error dialog at the user for the
-		// // first case, simply retry the query when the cursor is null.
-		// // Worst case, we end up doing the same query twice.
-		// if (mTrackCursor != null) {
-		// init(mTrackCursor, false);
-		// } else {
-		// setTitle(R.string.working_songs);
-		// getTrackCursor(mAdapter.getQueryHandler(), null, true);
-		// }
-		// }
 
 	}
 
+	//
+	// private Cursor getTrackCursor(TrackListAdapter.TrackQueryHandler
+	// queryhandler, String filter, boolean async) {
+	//
+	// if (queryhandler == null) {
+	// throw new IllegalArgumentException();
+	// }
+	//
+	// Cursor ret = null;
+	// mSortOrder = MediaStore.Audio.Media.TITLE_KEY;
+	// StringBuilder where = new StringBuilder();
+	// where.append(MediaStore.Audio.Media.TITLE + " != ''");
+	//
+	// if (mGenre != null) {
+	// Uri uri = MediaStore.Audio.Genres.Members.getContentUri("external",
+	// Integer.valueOf(mGenre));
+	// if (!TextUtils.isEmpty(filter)) {
+	// uri = uri.buildUpon().appendQueryParameter("filter",
+	// Uri.encode(filter)).build();
+	// }
+	// mSortOrder = MediaStore.Audio.Genres.Members.DEFAULT_SORT_ORDER;
+	// ret = queryhandler.doQuery(uri, mCursorCols, where.toString(), null,
+	// mSortOrder, async);
+	// } else if (mPlaylist != null) {
+	// if (mPlaylist.equals("nowplaying")) {
+	// if (MusicUtils.sService != null) {
+	// ret = new NowPlayingCursor(MusicUtils.sService, mCursorCols);
+	// if (ret.getCount() == 0) {
+	// finish();
+	// }
+	// } else {
+	// // Nothing is playing.
+	// }
+	// } else if (mPlaylist.equals("podcasts")) {
+	// where.append(" AND " + MediaStore.Audio.Media.IS_PODCAST + "=1");
+	// Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+	// if (!TextUtils.isEmpty(filter)) {
+	// uri = uri.buildUpon().appendQueryParameter("filter",
+	// Uri.encode(filter)).build();
+	// }
+	// ret = queryhandler.doQuery(uri, mCursorCols, where.toString(), null,
+	// MediaStore.Audio.Media.DEFAULT_SORT_ORDER, async);
+	// } else if (mPlaylist.equals("recentlyadded")) {
+	// // do a query for all songs added in the last X weeks
+	// Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+	// if (!TextUtils.isEmpty(filter)) {
+	// uri = uri.buildUpon().appendQueryParameter("filter",
+	// Uri.encode(filter)).build();
+	// }
+	// int X = MusicUtils.getIntPref(this, "numweeks", 2) * (3600 * 24 * 7);
+	// where.append(" AND " + MediaStore.MediaColumns.DATE_ADDED + ">");
+	// where.append(System.currentTimeMillis() / 1000 - X);
+	// ret = queryhandler.doQuery(uri, mCursorCols, where.toString(), null,
+	// MediaStore.Audio.Media.DEFAULT_SORT_ORDER, async);
+	// } else {
+	// Uri uri = MediaStore.Audio.Playlists.Members.getContentUri("external",
+	// Long.valueOf(mPlaylist));
+	// if (!TextUtils.isEmpty(filter)) {
+	// uri = uri.buildUpon().appendQueryParameter("filter",
+	// Uri.encode(filter)).build();
+	// }
+	// mSortOrder = MediaStore.Audio.Playlists.Members.DEFAULT_SORT_ORDER;
+	// ret = queryhandler.doQuery(uri, mPlaylistMemberCols, where.toString(),
+	// null, mSortOrder, async);
+	// }
+	// } else {
+	// if (mAlbumId != null) {
+	// where.append(" AND " + MediaStore.Audio.Media.ALBUM_ID + "=" + mAlbumId);
+	// mSortOrder = MediaStore.Audio.Media.TRACK + ", " + mSortOrder;
+	// }
+	// if (mArtistId != null) {
+	// where.append(" AND " + MediaStore.Audio.Media.ARTIST_ID + "=" +
+	// mArtistId);
+	// }
+	// where.append(" AND " + MediaStore.Audio.Media.IS_MUSIC + "=1");
+	// Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+	// if (!TextUtils.isEmpty(filter)) {
+	// uri = uri.buildUpon().appendQueryParameter("filter",
+	// Uri.encode(filter)).build();
+	// }
+	// ret = queryhandler.doQuery(uri, mCursorCols, where.toString(), null,
+	// mSortOrder, async);
+	// }
+	//
+	// // This special case is for the "nowplaying" cursor, which cannot be
+	// // handled
+	// // asynchronously using AsyncQueryHandler, so we do some extra
+	// // initialization here.
+	// if (ret != null && async) {
+	// init(ret, false);
+	// setTitle();
+	// }
+	// return ret;
+	// }
+	//
+
+	// 查询歌曲信息的ListView适配器
 	static class TrackListAdapter extends SimpleCursorAdapter {
 		boolean mIsNowPlaying;
 		boolean mDisableNowPlayingIndicator;
@@ -114,8 +179,12 @@ public class TrackBrowserActivity extends ListActivity {
 			char[] buffer2;
 		}
 
+		// A helper class to help make handling asynchronous ContentResolver
+		// queries easier
+		// 处理异步ContentResolver查询的帮助类
 		class TrackQueryHandler extends AsyncQueryHandler {
 
+			// 查询的参数
 			class QueryArgs {
 				public Uri uri;
 				public String[] projection;
@@ -153,9 +222,24 @@ public class TrackBrowserActivity extends ListActivity {
 					args.selectionArgs = selectionArgs;
 					args.orderBy = orderBy;
 					// 交给handler处理查询,异步查询
+					/**
+					 * public void startQuery(int token, Object cookie, Uri uri,
+					 * String[] projection, String selection, String[]
+					 * selectionArgs, String orderBy)
+					 * 
+					 * token: A token passed into onQueryComplete to identify
+					 * the query.
+					 * 
+					 * cookie: An object that gets passed into onQueryComplete
+					 * 
+					 * This method begins an asynchronous query. When the query
+					 * is done onQueryComplete is called.
+					 */
+					// 执行异步查询，查询结果args传给 onQueryComplete()
 					startQuery(0, args, limituri, projection, selection, selectionArgs, orderBy);
 					return null;
 				}
+
 				return MusicUtils.query(mActivity, uri, projection, selection, selectionArgs, orderBy);
 			}
 
@@ -170,7 +254,20 @@ public class TrackBrowserActivity extends ListActivity {
 
 			@Override
 			protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+				// 查询完成
+				Log.i("@@@", "query complete: " + cursor.getCount() + " " + mActivity);
 				mActivity.init(cursor, cookie != null);// 当cookie!=null说明limit是100，不止一条记录
+
+				/**
+				 * token==0 执行的是异步查询
+				 * 
+				 * cookie the cookie object passed in from startQuery.
+				 * 
+				 * 
+				 */
+				if (token == 0 && cookie != null && cursor != null && !cursor.isClosed() && cursor.getCount() >= 100) {
+
+				}
 
 			}
 
